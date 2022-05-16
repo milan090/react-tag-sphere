a37ae9cc3697326e234f0310ed95e9988f6344c6
milan090 <milanmuhammed1@gmail.com>
2022-05-16 18:34:35 +0530

    init

---
+++ example/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { TagSphere } from "react-tag-sphere";
+import "./App.css";
+
+function App() {
+  return <TagSphere keepRollingAfterMouseOut={true} blur={false} />;
+}
+
+export default App;
---
+++ lib/esm/index.d.ts
@@ -0,0 +1,32 @@
+import React from "react";
+import { CSSProperties, ReactNode } from "react";
+declare type TagSphereProps = {
+    /** Can be list of strings, html elements or react components */
+    texts: (string | ReactNode)[];
+    /** @default texts.length * 15 */
+    radius?: number;
+    /**@default 7 */
+    maxSpeed: number;
+    /** @default 32 */
+    initialSpeed: number;
+    /** In degrees
+     * @default 135
+     */
+    initialDirection: 135;
+    /** @default true */
+    keepRollingAfterMouseOut: boolean;
+    className?: string;
+    style?: CSSProperties;
+    /** @default true */
+    useContainerInlineStyles: boolean;
+    fullWidth: boolean;
+    fullHeight: boolean;
+    /** Make the items highlightable with selection
+     * @default false
+     */
+    userSelect: boolean;
+    /** @default true */
+    blur: boolean;
+};
+export declare const TagSphere: React.FC<Partial<TagSphereProps>>;
+export {};
